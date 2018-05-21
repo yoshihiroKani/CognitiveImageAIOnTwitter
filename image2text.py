289c964ab6b763bd73c0b462cc4b32b6cb939740
yoshihiroKani <yoshihiro.kani.1223@gmail.com>
2018-05-21 14:09:19 +0900

    Update image2text.py

--- image2text.py
+++ image2text.py
@@ -13,7 +13,8 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 #ssl_ignore--<<
 
-#var_declaration_for_twitter_API-->>
+#twitterAPIの利用-->>
+##アクセスTokenなど接続情報の指定-->>
 consumer_key = "XXXXXXXXXXXXXXXXXXXXXXXXXX"
 consumer_secret = "XXXXXXXXXXXXXXXXXXXXXXXXXX"
 access_token = "XXXXXXXXXXXXXXXXXXXXXXXXXX"
@@ -24,9 +25,9 @@ api = tweepy.API(auth)
 f = open('sinceId.txt')
 file_id = f.readline()
 f.close()
-#var_declaration_for_twitter_API--<<
 
-#var_declaration_for_computer_vision_API-->>
+#Microsoft Cognitive ComputerVisionAPIの利用-->>
+##接続情報の指定-->>
 image_url = ''
 headers = {
         'Content-Type': 'application/json',
@@ -35,17 +36,17 @@ headers = {
 params = urllib.urlencode({
         'visualFeatures': 'Description',
 })
-#var_declaration_for_computer_vision_API--<<
 
-#google_cloud_translation_API-->>
+#GoogleTranslateAPIの利用-->>
+##接続情報の指定-->>
 gct_head = 'https://www.googleapis.com/language/translate/v2?key='
 gct_key = 'XXXXXXXXXXXXXXXXXXXXXXXXXX'
 gct_mid = '&target=ja&q='
 gct_url = gct_head + gct_key + gct_mid
-#google_cloud_translation_API--<<
 
 #body-->>
 conn = httplib.HTTPSConnection('api.projectoxford.ai')
+#sinceIdファイルのID以降のIDを持つ自分へのリプライツイートを指定
 status = api.mentions_timeline(since_id=file_id)
 try:
         if status[0]._json['id_str'] != '':
@@ -57,16 +58,16 @@ try:
                 f.close()
 except IndexError:
         pass
-for foo in range(len(status)):
+for index in range(len(status)):
         body = ""
         try:
-                image_url = status[foo]._json['entities']['media'][0]['media_url']
-                print('img_url' + str(foo) + ' >> ' + image_url)
+                image_url = status[index]._json['entities']['media'][0]['media_url']
+                print('img_url' + str(index) + ' >> ' + image_url)
                 body = """{'url': '%s'}""" % (image_url)
         except KeyError:
                 print("microsoft computer vision api KeyError")
                 continue
-        scrn_name = status[foo]._json[u'user'][u'screen_name']
+        scrn_name = status[index]._json[u'user'][u'screen_name']
         conn.request("POST", "/vision/v1.0/analyze?%s" % params, body, headers)
         cv_res = conn.getresponse()
         cv_res_data = cv_res.read()
@@ -81,7 +82,7 @@ for foo in range(len(status)):
         print(gct_req_url)
         gct_res_txt = gct_res.json()[u'data'][u'translations'][0][u'translatedText']
         rep_txt = '@' + scrn_name + ' ' + gct_res_txt
-        rep_id = status[foo]._json['id_str']
+        rep_id = status[index]._json['id_str']
         api.update_status(status=rep_txt, in_reply_to_status_id=rep_id)
 #       print(gct_res_txt)
 conn.close()
